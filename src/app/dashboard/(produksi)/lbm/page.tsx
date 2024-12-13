b985ac236869eb566ab12e63d3b18a76d286eaa8
Redho Arifin <redhoarifin2017@gmail.com>
2024-12-13 11:31:26 +0700

    remove back button

--- src/app/dashboard/(produksi)/lbm/page.tsx
+++ src/app/dashboard/(produksi)/lbm/page.tsx
@@ -1,13 +1,11 @@
-import BackButton from "@/components/back-button";
+
 import DataLBMPage from "@/components/lbm/page";
 import * as React from "react";
 
 export default function Page() {
   return (
     <>
-     
-            <BackButton />
-           
+ 
       <div className="justify-center flex px-4">
 
       <DataLBMPage />
