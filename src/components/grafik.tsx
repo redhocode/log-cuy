281ea5173289d2a1f5d6a48d96dd47634ff651ce
Redho Arifin <redho.arn@gmail.com>
2025-02-18 13:55:18 +0700

    disabeld eslint

--- src/components/grafik.tsx
+++ src/components/grafik.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
