0f5514fc7f4c84dbfcdf4010557832ad29fc2bcb
Redho Arifin <redhoarifin2017@gmail.com>
2025-04-24 13:56:37 +0700

    add id telegram lutfi

--- src/app/api/notif/exim/route.ts
+++ src/app/api/notif/exim/route.ts
@@ -11,7 +11,7 @@ interface TelegramResponse {
 export async function POST(req: NextRequest) {
   const { message } = await req.json(); // Mendapatkan data JSON dari request
 
-  const chatIds = ["904514717"]; // Daftar chat IDs yang ingin dikirimi pesan
+  const chatIds = ["904514717","8001360289"]; // Daftar chat IDs yang ingin dikirimi pesan
   // "6551726602", "5338845190", "6024064758","904514717"
   const token = "7880924007:AAHvKkqrvKA45d6fLM0huUD1-H4O_hRrtW8"; // Ganti dengan token bot Telegram Anda
 
