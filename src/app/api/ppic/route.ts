b28a771870dcb63b46eef03a943f52653fee2e91
Redho Arifin <redho.arn@gmail.com>
2026-02-05 08:44:32 +0700

    update version

--- src/app/api/ppic/route.ts
+++ src/app/api/ppic/route.ts
@@ -13,7 +13,7 @@ export async function GET(request: Request) {
     const pool = await getPool();
 
     let query = `
-      SELECT TOP (20)
+      SELECT TOP (100)
         hd.[OrderID] AS No_SPK,
         hd.[OrderDate] AS Tanggal_Order,
         hd.[Remark] AS Nama_PO,
